bb326b13fa5ac7198c5aceecac112db0f6cb4910
Georgi Grigorov <georgi.grigorov@limechain.tech>
2024-11-01 16:09:23 +0200

    feat: extended CurrentEpochReader to decode all params from runtime response

--- src/main/java/com/limechain/babe/api/scale/CurrentEpochReader.java
+++ src/main/java/com/limechain/babe/api/scale/CurrentEpochReader.java
@@ -1,15 +1,22 @@
 package com.limechain.babe.api.scale;
 
 import com.limechain.babe.state.CurrentEpoch;
+import com.limechain.chain.lightsyncstate.scale.AuthorityReader;
 import io.emeraldpay.polkaj.scale.ScaleCodecReader;
 import io.emeraldpay.polkaj.scale.ScaleReader;
+import io.emeraldpay.polkaj.scale.reader.ListReader;
 import io.emeraldpay.polkaj.scale.reader.UInt64Reader;
 
-// Note: runtime call returns also epoch length, authority list and randomness
 public class CurrentEpochReader implements ScaleReader<CurrentEpoch> {
 
     @Override
     public CurrentEpoch read(ScaleCodecReader reader) {
-        return new CurrentEpoch(new UInt64Reader().read(reader), new UInt64Reader().read(reader));
+        return CurrentEpoch.builder()
+                .epochIndex(new UInt64Reader().read(reader))
+                .epochStartingSlot(new UInt64Reader().read(reader))
+                .epochLength(new UInt64Reader().read(reader))
+                .authorityList(reader.read(new ListReader<>(new AuthorityReader())))
+                .randomness(reader.readUint256())
+                .build();
     }
 }
--- src/main/java/com/limechain/babe/state/CurrentEpoch.java
+++ src/main/java/com/limechain/babe/state/CurrentEpoch.java
@@ -1,13 +1,18 @@
 package com.limechain.babe.state;
 
-import lombok.AllArgsConstructor;
+import com.limechain.chain.lightsyncstate.Authority;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Getter
-@AllArgsConstructor
+@Builder
 public class CurrentEpoch {
     private BigInteger epochIndex;
     private BigInteger epochStartingSlot;
+    private BigInteger epochLength;
+    private List<Authority> authorityList;
+    private byte[] randomness;
 }
