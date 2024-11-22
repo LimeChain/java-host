2d8dc8d8c4e184edcdd30df21f96c8dc485f7c73
Yordan Atanasov <yordan.atanasov@limechain.tech>
2024-11-22 15:59:46 +0200

    chore: Fix sonar issue.

--- src/main/java/com/limechain/babe/dto/EpochSlot.java
+++ src/main/java/com/limechain/babe/dto/EpochSlot.java
@@ -2,12 +2,13 @@ package com.limechain.babe.dto;
 
 import lombok.Value;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.Duration;
 import java.time.Instant;
 
 @Value
-public class EpochSlot {
+public class EpochSlot implements Serializable {
 
     Instant start;
     Duration duration;
