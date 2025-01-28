8506b5c7a2687d4ae09e785f82e267f9d52c66d5
Oleksandr <sanyaserebro17@gmsil.com>
2025-01-28 10:14:16 +0200

    Implemented broadcasting Vote Message

---
+++ src/main/java/com/limechain/network/protocol/grandpa/messages/vote/FullVoteScaleWriter.java
@@ -0,0 +1,27 @@
+package com.limechain.network.protocol.grandpa.messages.vote;
+
+import com.limechain.network.protocol.grandpa.messages.commit.VoteScaleWriter;
+import io.emeraldpay.polkaj.scale.ScaleCodecWriter;
+import io.emeraldpay.polkaj.scale.ScaleWriter;
+import io.emeraldpay.polkaj.scale.writer.UInt64Writer;
+
+import java.io.IOException;
+
+public class FullVoteScaleWriter implements ScaleWriter<FullVote> {
+
+    private final UInt64Writer uint64Writer;
+    private final VoteScaleWriter voteScaleWriter;
+
+    public FullVoteScaleWriter(UInt64Writer uint64Writer, VoteScaleWriter voteScaleWriter) {
+        this.uint64Writer = uint64Writer;
+        this.voteScaleWriter = voteScaleWriter;
+    }
+
+    @Override
+    public void write(ScaleCodecWriter writer, FullVote fullVote) throws IOException {
+        writer.writeByte(fullVote.getStage().getStage());
+        voteScaleWriter.write(writer, fullVote.getVote());
+        uint64Writer.write(writer, fullVote.getRound());
+        uint64Writer.write(writer, fullVote.getSetId());
+    }
+}
