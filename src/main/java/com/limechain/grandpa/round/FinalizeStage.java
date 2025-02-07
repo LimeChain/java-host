c6d562538914f28fd4e0c1163b3f9eb453e9f800
Hristiyan Mitov <hristiyan.mitov@limechain.tech>
2025-02-07 14:43:46 +0200

    feat: Implement Finalize stage in Play-Grandpa-Round.

--- src/main/java/com/limechain/grandpa/round/FinalizeStage.java
+++ src/main/java/com/limechain/grandpa/round/FinalizeStage.java
@@ -1,13 +1,46 @@
 package com.limechain.grandpa.round;
 
+import com.limechain.network.protocol.warp.dto.BlockHeader;
+import lombok.extern.java.Log;
+
+@Log
 public class FinalizeStage implements StageState {
 
     @Override
     public void start(GrandpaRound round) {
+
+        log.fine(String.format("Round %d entered the Finalize stage.", round.getRoundNumber()));
+
+        if (isRoundReadyToEnd(round)) {
+            end(round);
+            return;
+        }
+
+        Runnable onFinalizeHandler = () -> {
+            if (isRoundReadyToEnd(round)) {
+                end(round);
+            }
+        };
+        round.setOnFinalizeHandler(onFinalizeHandler);
     }
 
     @Override
     public void end(GrandpaRound round) {
+        log.info(String.format("Round %d met finalization conditions and will be finalized.", round.getRoundNumber()));
+        round.setOnFinalizeHandler(null);
+        round.attemptToFinalizeAt();
+        log.fine(String.format("Round %d exits Finalize stage.", round.getRoundNumber()));
+
+        round.end();
+    }
+
+    private boolean isRoundReadyToEnd(GrandpaRound round) {
+        BlockHeader finalized = round.getFinalizedBlock();
+        BlockHeader prevBestFinalCandidate = round.getPrevBestFinalCandidate();
+
+        if (finalized == null || prevBestFinalCandidate == null) {
+            return false;
+        }
+        return finalized.getBlockNumber().compareTo(prevBestFinalCandidate.getBlockNumber()) >= 0;
     }
 }
-
